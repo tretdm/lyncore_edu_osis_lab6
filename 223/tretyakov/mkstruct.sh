fa4e8589f21450333cd8f9ad44c24fd7daf96f80
Dmitry Tretyakov <tretdm@inbox.ru>
2024-11-13 16:38:15 +0700

    Update mkstruct.sh

--- 223/tretyakov/mkstruct.sh
+++ 223/tretyakov/mkstruct.sh
@@ -3,6 +3,6 @@ if [[ $1 ]]; then
 	mkdir -p ../../$1/src/images
 	mkdir -p ../../$1/src/scripts
 	mkdir -p ../../$1/src/styles
-	cp README.md ../../$1
+	cp ../../README.md ../../$1
 	touch .gitignore
 fi
