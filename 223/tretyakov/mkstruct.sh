eb6cbda8811ad0a808011cbd00f479361581cd1b
Dmitry Tretyakov <tretdm@inbox.ru>
2024-11-13 16:32:18 +0700

    Доделал

--- 223/tretyakov/mkstruct.sh
+++ 223/tretyakov/mkstruct.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [[ $1 && $2 ]]; then
 	if [[ $1 == "scripts" || $1 == "styles" || $1 == "images" ]]; then
-		mkdir -p ../$1/$2
-		cd ../$1/$2
+		mkdir -p ../../$1/$2
+		cd ../../$1/$2
 	fi
 fi
