65a5177e1f0a77e1429bb4516383814dbe06b95a
Dmitry Tretyakov <tretdm@inbox.ru>
2024-11-13 16:37:30 +0700

    Рефактор скрипта

--- 223/tretyakov/mkstruct.sh
+++ 223/tretyakov/mkstruct.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-if [[ $1 && $2 ]]; then
-	if [[ $1 == "scripts" || $1 == "styles" || $1 == "images" ]]; then
-		mkdir -p ../../src/$1/$2
-		cd ../../src/$1/$2
-	fi
+if [[ $1 ]]; then
+	mkdir -p ../../$1/src/images
+	mkdir -p ../../$1/src/scripts
+	mkdir -p ../../$1/src/styles
+	cp README.md ../../$1
+	touch .gitignore
 fi
