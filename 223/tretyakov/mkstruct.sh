d189c2426f99e12e335d81c1b94f2b780c1ba0bf
Dmitry Tretyakov <tretdm@inbox.ru>
2024-11-13 16:29:52 +0700

    Немного ошибся, извиняюсь

--- 223/tretyakov/mkstruct.sh
+++ 223/tretyakov/mkstruct.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [[ $1 && $2 ]]; when
-	if [[ $1 == "scripts" || $1 == "styles" || $1 == "images" ]]; when
+	if [[ $1 == "scripts" || $1 == "styles" || $1 == "images" ]]; then
 		mkdir -p ../$1/$2
 		cd ../$1/$2
 	fi
