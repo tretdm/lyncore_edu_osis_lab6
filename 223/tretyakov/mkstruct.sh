689d3458bca51b9d907c2545589e2217ebab2a22
Dmitry Tretyakov <tretdm@inbox.ru>
2024-11-15 17:51:30 +0700

    Fix

--- 223/tretyakov/mkstruct.sh
+++ 223/tretyakov/mkstruct.sh
@@ -3,7 +3,5 @@ if [[ $1 ]]; then
 	mkdir -p ../../$1/src/images
 	mkdir -p ../../$1/src/scripts
 	mkdir -p ../../$1/src/styles
-	touch ../../README.md
-	touch ../../.gitignore
 	echo "Project is created."
 fi
